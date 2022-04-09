bc40f0a87c6aa8362146f4a114c780b0911f0973
Julio Jimenez <julio@julioj.com>
2022-04-09 10:57:38 -0400

    all-your-base
    Signed-off-by: Julio Jimenez <julio@julioj.com>

---
+++ hello-6087/hello-6087-1.c
@@ -0,0 +1,10 @@
+/*  Hello, 6.087 students!
+    Created by Julio Jimenez, 03/24/2022 */
+
+#include <stdio.h> /* basic i/o facilities */
+
+int main(void) {
+    const char msg[] = "Hello, 6.087 students!";
+    puts(msg);
+    return 0;
+}
