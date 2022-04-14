f67e942585c9ebef85cba1303775bcd1d7f41930
Julio Jimenez <julio@julioj.com>
2022-04-13 20:55:37 -0400

    define
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- chapter-1/temp-for.c
+++ chapter-1/temp-for.c
@@ -1,7 +1,10 @@
+#define MIN 0
+#define MAX 300
+#define STEP 20
 #include <stdio.h>
 
 main() {
   int fahr;
-  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+  for (fahr = MIN; fahr <= MAX; fahr = fahr + STEP)
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
